7f11f520a6ad95f03bebf75b9305261c226de2ea
traxxis-coder <cigatomas@gmail.com>
2025-11-19 17:10:58 +0100

    Set up the default layout

---
+++ script.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+let firstArg;
+let secondArg;
+let operand;
+
+function operate(a, b, sign) {
+    switch (sign) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "*":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+        break;
+    };       
+};
