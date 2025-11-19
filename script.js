e15d135fdec6293c724d05a2b6af7b996a1985c2
traxxis-coder <cigatomas@gmail.com>
2025-11-19 20:51:39 +0100

    Remove code repetition to improve readability

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const btnValues = {
+    "numbers": {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+        "zero": "0"
+    },
+    "operators": {
+        "add": "+",
+        "subtract": "-",
+        "divide": "/",
+        "multiply": "*"
+    }
+}
+
 function add(a, b) {
     return a + b;
 };
@@ -36,146 +57,35 @@ const display = document.querySelector("#display p");
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        switch (button.id) {
-            case "one":
-                if (operator) {
-                    secondArg += "1";
-                    display.textContent = secondArg;
-                } else {
-                    firstArg += "1";
-                    display.textContent = firstArg;
-                };
-                break;
-            case "two":
-                if (operator) {
-                    secondArg += "2";
-                    display.textContent = secondArg;
-                } else {
-                    firstArg += "2";
-                    display.textContent = firstArg;
-                };                break;
-            case "three":
-                if (operator) {
-                    secondArg += "3";
-                    display.textContent = secondArg;
-                } else {
-                    firstArg += "3";
-                    display.textContent = firstArg;
-                };
-                break;
-            case "four":
-                if (operator) {
-                    secondArg += "4";
-                    display.textContent = secondArg;
-                } else {
-                    firstArg += "4";
-                    display.textContent = firstArg;
-                };
-                break;
-            case "five":
-                if (operator) {
-                    secondArg += "5";
-                    display.textContent = secondArg;
-                } else {
-                    firstArg += "5";
-                    display.textContent = firstArg;
-                };
-                break;
-            case "six":
-                if (operator) {
-                    secondArg += "6";
-                    display.textContent = secondArg;
-                } else {
-                    firstArg += "6";
-                    display.textContent = firstArg;
-                };
-                break;
-            case "seven":
-                if (operator) {
-                    secondArg += "7";
-                    display.textContent = secondArg;
-                } else {
-                    firstArg += "7";
-                    display.textContent = firstArg;
-                };
-                break;
-            case "eight":
-                if (operator) {
-                    secondArg += "8";
-                    display.textContent = secondArg;
-                } else {
-                    firstArg += "8";
-                    display.textContent = firstArg;
-                };
-                break;
-            case "nine":
-                if (operator) {
-                    secondArg += "9";
-                    display.textContent = secondArg;
-                } else {
-                    firstArg += "9";
-                    display.textContent = firstArg;
-                };
-                break;
-            case "zero":
-                if (operator && secondArg != "") {
-                    secondArg += "0";
-                    display.textContent = secondArg;
-                } else if (firstArg != ""){
-                    firstArg += "0";
-                    display.textContent = firstArg;
-                };
-                break;
-            case "add":
-                if (secondArg) {
-                    let result = operate(firstArg, secondArg, operator);
-                    firstArg = result;
-                    display.textContent = firstArg;
-                    secondArg = "";
-                }
-                operator = "+";
-                break;
-            case "subtract":
-                if (secondArg) {
-                    let result = operate(firstArg, secondArg, operator);
-                    firstArg = result;
-                    display.textContent = firstArg;
-                    secondArg = "";
-                }
-                operator = "-";
-                break;
-            case "multiply":
-                if (secondArg) {
-                    let result = operate(firstArg, secondArg, operator);
-                    firstArg = result;
-                    display.textContent = firstArg;
-                    secondArg = "";
-                }
-                operator = "*";
-                break;
-            case "divide":
-                if (secondArg) {
-                    let result = operate(firstArg, secondArg, operator);
-                    firstArg = result;
-                    display.textContent = firstArg;
-                    secondArg = "";
-                }
-                operator = "/";
-                break;
-            case "equals":
-                if (secondArg) {
-                    let result = operate(firstArg, secondArg, operator);
-                    firstArg = result;
-                    display.textContent = firstArg;
-                    secondArg = "";
-                    operator = "";
-                }
-                break;
-            case "clear":
-                firstArg = "";
+        if (button.classList.contains("number")) {
+            if (operator) {
+                secondArg += btnValues.numbers[button.id];
+                display.textContent = secondArg;
+            } else {
+                firstArg += btnValues.numbers[button.id];
+                display.textContent = firstArg;
+            };
+        } else if (button.classList.contains("operator")) {
+            if (secondArg) {
+                let result = operate(firstArg, secondArg, operator);
+                firstArg = result;
+                display.textContent = firstArg;
+                secondArg = "";
+            }
+            operator = btnValues.operators[button.id];
+        } else if (button.id == "equals") {
+            if (secondArg) {
+                let result = operate(firstArg, secondArg, operator);
+                firstArg = result;
+                display.textContent = firstArg;
                 secondArg = "";
                 operator = "";
-                display.textContent = "0";
-        }
-    })
-})
+            }
+        } else if(button.id == "clear") {
+            firstArg = "";
+            secondArg = "";
+            operator = "";
+            display.textContent = "0";
+        };
+    });
+});
